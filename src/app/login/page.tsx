c77ec0d7c979c588231af3daf414262fbb37739f
Anthomate <anthony.sapet@outlook.com>
2024-02-08 09:33:27 +0100

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import LoginForm from "@/components/form/login-form";
 
 export default function LoginPage() {
     return (
@@ -7,30 +8,7 @@ export default function LoginPage() {
                 <title>Login Page</title>
             </Head>
             <div className="flex items-center justify-center h-screen bg-gray-100">
-                <div className="w-full max-w-xs">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                                Email
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Password
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                Sign In
-                            </button>
-                            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                                Forgot Password?
-                            </a>
-                        </div>
-                    </form>
-                </div>
+                <LoginForm />
             </div>
         </>
     )
