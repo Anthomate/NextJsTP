6f4ea9c63d8681d542f07407c56a075f3eccf0f7
Anthomate <anthony.sapet@outlook.com>
2024-02-08 10:31:17 +0100

    commit

--- src/components/form/login-form.tsx
+++ src/components/form/login-form.tsx
@@ -31,8 +31,7 @@ export default function LoginForm()
                            type="password"
                            name="password"
                            placeholder="******************"
-                           required
-                           minLength={6} />
+                           required />
                 </div>
                 <div className="flex items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
